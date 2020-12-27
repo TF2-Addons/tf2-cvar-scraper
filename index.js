6499c14d2aac6708a043a1693f130e34ab878905
Techdoodle <chris@techchrism.me>
2020-12-27 15:42:31 -0800

    initial working version

---
+++ index.js
@@ -0,0 +1,44 @@
+const fs = require('fs').promises;
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
+
+async function getWikiBody()
+{
+    return (await fetch('https://developer.valvesoftware.com/wiki/List_of_TF2_console_commands_and_variables')).text();
+}
+
+async function scrapeData()
+{
+    const data = await getWikiBody();
+    const $ = cheerio.load(data);
+    
+    const version = $('#mw-content-text span').first().text();
+    
+    const cvars = [];
+    $('pre').each(function(i, el)
+    {
+        const lines = $(this).text().split('\n').filter(line => line.length > 0);
+        for(const line of lines)
+        {
+            const parts = line.split(':');
+            const [name, defValue, flags] = parts.splice(0, 3).map(v => v.trim());
+            const desc = parts.join(':').trim();
+            
+            cvars.push({
+                name,
+                defValue,
+                flags: flags.split(', ').filter(v => v.length !== 0)
+                            .map(v => v.substr(1, v.length - 2)),
+                desc
+            });
+        }
+    });
+    return {cvars, version};
+}
+
+(async () =>
+{
+    const {cvars, version} = await scrapeData();
+    console.log(`Loaded ${cvars.length} cvars ${version}`);
+    await fs.writeFile('./cvars.json', JSON.stringify(cvars));
+})();
